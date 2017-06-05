54b4ea808521a9e60986bf33a1260cb6b6de686e
Florian Kruse <flokru@googlemail.com>
2017-06-05 13:44:09 +0200

    More robust: pinging instead or URL loading

--- internet_test.py
+++ internet_test.py
@@ -1,38 +1,29 @@
 #!/usr/bin/env python3
 
-from urllib import request
-#from socket import timeout
 import time
 from datetime import datetime
 import sys
+import subprocess
 
-TEST_URIS = ["http://www.google.com", "http://www.facebook.com", "http://www.amazon.com"]
+TEST_HOSTS = ["www.google.com", "www.facebook.com", "www.amazon.com"]
 TIMEOUT = 2
-MAX_BUFFER_LEN = 100
 WAIT_TIME = 15
 file_num = 1
 results = []
 
-def fetch_any_site():
-	for uri in TEST_URIS:
-		#try:
-			print 'probing', uri, TIMEOUT
-			response = request.urlopen(uri, timeout = TIMEOUT)
-			return True
-		#except (urllib2.URLError, timeout) as err: pass
-	return False
+def check_ping(hostname):
+	try:
+		output = subprocess.check_output("ping -c 1 " + hostname, shell=True, stderr=subprocess.PIPE)
+		return True
+	except Exception:
+		return False
 
-def flush_results():
-	global file_num
-	global results
-	
-	filename = "internet_status_" + str(file_num) + ".csv"
-	file_num = file_num + 1
-	file_handle = open(filename, "w")
-	for line in results:
-		file_handle.write(line + "\n")
-	file_handle.close()
-	results = []
+def fetch_any_site():
+	for host in TEST_HOSTS:
+		result_ping = check_ping(host)
+		if result_ping is True:
+			return True
+	return False
 
 def check_internet():
 	global results
@@ -42,12 +33,8 @@ def check_internet():
 	status = "connected" if is_connected else "disconnected"
 	
 	result_str = status + "," + timestamp
-	print result_str
+	print(result_str)
 	sys.stdout.flush()
-	#results.append(result_str)
-
-	#if len(results) >= MAX_BUFFER_LEN:
-	#	flush_results()
 
 def loop():
 	check_internet()
